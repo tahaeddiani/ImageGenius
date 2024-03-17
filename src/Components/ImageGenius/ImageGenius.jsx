c7efad1d3ddef7431a969fdc76f11be877e58238
tahaeddiani <tahaeddiani2@gmail.com>
2024-03-17 22:39:13 +0000

    orgnized the code(added some comments in jsx and desigend the search box

--- src/Components/ImageGenius/ImageGenius.jsx
+++ src/Components/ImageGenius/ImageGenius.jsx
@@ -4,13 +4,19 @@ import img_default from '../Assets/img-default.png'
 const ImageGenius = () => {
   return (
     <div className='ai-image-genius'>
+
+      {/* title*/}
       <div className="header">Text To Image <span>AI</span></div>
+
+      {/* image*/}
       <div className="img-loading">
         <div className="image"><img src={img_default} alt="" /></div>
       </div>
 
-      <div className="seach-box">
+      {/* search box*/}
+      <div className="search-box">
         <input type="text" className='search-input' placeholder='Describe Your image...' />
+        <div className="generate-btn">Generatee</div> 
       </div>
       
     </div>
