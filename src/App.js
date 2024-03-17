ebb04a3fa1b6e7ab2a6d973905d814785d29e8b2
tahaeddiani <tahaeddiani2@gmail.com>
2024-03-17 22:00:53 +0000

    added the ImageGenius too app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import ImageGenius from './Components/Assets/ImageGenius/ImageGenius';
 
 function App() {
   return (
     <div>
+      <ImageGenius/>
 
     </div>
   );
---
+++ src/Components/ImageGenius/ImageGenius.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import 'ImageGenius.css'
+const ImageGenius = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ImageGenius
